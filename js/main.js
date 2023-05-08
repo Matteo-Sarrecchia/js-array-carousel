71755f2ebf703748dc34ff2a5dfe4abf7e376ff5
Matteo Sarrecchia <morrea@hotmail.it>
2023-05-08 18:15:55 +0200

    modifiche click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // variabili globali
 const container = document.getElementById("container");
-const arrow = document.querySelector('.arrow');
+let arrowNext = document.querySelector('.arrowNext');
+let arrowPrev = document.querySelector(".arrowPrev")
+let square;
+
 let positionListImg = 0
 
 // creare array con immagini
@@ -8,15 +11,69 @@ let listImg = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp", ];
 console.log(listImg);
 
 for (i = 0; i < listImg.length; i++){
-    const square = document.createElement("div");
+    square = document.createElement("div");
     container.append(square);
+    square.innerHTML = `<img src="img/${listImg[positionListImg++]}">`;
+
+    // aggiunta classe stile css
     square.classList.add("item")
-    square.innerHTML = `<img src="img/${listImg[positionListImg++]}"><div class="arrow"></div>`;
+
+    
 }
 
-arrow.addEventListener("click",
-    function (){
-        console.log("ciao")
+
+let positionItem = 0;
+
+let item = document.querySelectorAll(".item");
+
+console.log(item);
+
+item[positionItem].classList.add("active");
+
+
+arrowNext.addEventListener("click",
+    
+    function(){
+
+        if(positionItem < listImg.length -1){
+
+            // tolgo la classe active al primo elemento
+        item[positionItem].classList.remove("active");
+       
+        // passo all'elemento successivo
+        positionItem++
+        
+        // aggiungo la classe active all'elemento successivo
+        item[positionItem].classList.add("active");
+
+        // se sono all'ultimo elemento
+        if (positionItem === listImg.length -1){
+           arrowNext.classList.add("hidden");
+        }
+        }     
     }
 )
 
+arrowPrev.addEventListener("click",
+
+    function(){
+
+        if(positionItem < listImg.length -1){
+
+            // tolgo la classe active al primo elemento
+        item[positionItem].classList.remove("active");
+        
+        // passo all'elemento successivo
+        positionItem--
+        
+        // aggiungo la classe active all'elemento successivo
+        item[positionItem].classList.add("active");
+
+        // se sono all'ultimo elemento
+        if (positionItem === 0){
+            arrowPrev.classList.add("hidden");
+        }
+        }     
+    }
+    
+)
